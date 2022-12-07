f36acacf6b5526599aff89427dce985cae7f1118
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-07 19:18:55 -0300

    second commit

--- interface.py
+++ interface.py
@@ -6,8 +6,12 @@ layout = [
     [sg.Text('Usuário'), sg.Input(key = 'usuario', size=(20, 1))],
     [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))],
     [sg.Text('Email'), sg.Input(key='email', size=(20, 1))],
+    [sg.Text('Telefone'), sg.Input(key='telefone', size=(20, 1))],
     [sg.Checkbox('Deseja salvar as informações de login?')],
     [sg.Button('Login')] 
 ]
+
+janela = sg.Window('Cadastro', layout)
+
 
 
